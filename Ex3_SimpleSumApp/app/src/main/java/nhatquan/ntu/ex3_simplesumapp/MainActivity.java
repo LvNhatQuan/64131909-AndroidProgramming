7a484b9c8d474ace65ab1a428b5e56fc02f696d7
LvNhatQuan <nhatquanb@gmail.com>
2025-03-12 22:45:45 +0700

    sửa lỗi

--- Ex3_SimpleSumApp/app/src/main/java/nhatquan/ntu/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/nhatquan/ntu/ex3_simplesumapp/MainActivity.java
@@ -1,9 +1,11 @@
 package nhatquan.ntu.ex3_simplesumapp;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //Đây là bộ lắng nghe và xử lý sự kiện click trên nút tính tổng
+    public void XuLyCong(View view){
+        // Tìm, tham chiếu đến điều khiển trên tệp XML, mapping sang java
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        // Lấy dữ liệu về ở điều khiển số A
+        String strA= editTextSoA.getText().toString();
+        // Lấy dữ liệu về ở điều khiển số B
+        String strB= editTextSoB.getText().toString();
+        //Chuyển dữ liệu sang dạng số
+        int so_A= Integer.parseInt(strA);
+        int so_B= Integer.parseInt(strB);
+        //Tính toán theo yêu cầu
+        int tong= so_A + so_B;
+        String strTong= String.valueOf(tong);//chuyển sang dạng chuỗi
+        //Hiện ra màn hình
+        editTextKetQua.setText(strTong);
     }
 }
