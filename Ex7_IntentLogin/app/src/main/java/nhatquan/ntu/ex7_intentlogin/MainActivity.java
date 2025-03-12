0165617dee3b1e1e393bb5df6838aa7826c387bb
LvNhatQuan <nhatquanb@gmail.com>
2025-03-12 22:30:35 +0700

    Main

---
+++ Ex7_IntentLogin/app/src/main/java/nhatquan/ntu/ex7_intentlogin/MainActivity.java
@@ -0,0 +1,26 @@
+package nhatquan.ntu.ex7_intentlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //Tìm đk
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //Sự kiện
+
+    }
+}
