8e992668f7a5ac6aca258e2b6786b11ca898e3e3
LvNhatQuan <nhatquanb@gmail.com>
2025-03-12 22:38:26 +0700

    Login

---
+++ Ex7_IntentLogin/app/src/main/java/nhatquan/ntu/ex7_intentlogin/LoginActivity.java
@@ -0,0 +1,53 @@
+package nhatquan.ntu.ex7_intentlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText edTenDN = (EditText) findViewById(R.id.edtUsername);
+                EditText edPass = (EditText) findViewById(R.id.edtPass);
+                String tenDangNhap = edTenDN.getText().toString();
+                String mk = edPass.getText().toString();
+                //Kiểm tra mật khẩu
+                if (tenDangNhap.equals("nhatquan") && mk.equals("177"))//mk đúng
+                {//chuyển sang màn hình home
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    //Gọi dữ liệu vào iQuiz, dạng key - value; key được dùng để bên kia lọc ra dữ liệu
+                    iQuiz.putExtra("ten_dang_nhap",tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap",mk);
+                    //Gửi đi
+                    startActivity(iQuiz);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "BẠN NHẬP SAI THÔNG TIN",Toast.LENGTH_LONG);
+                }
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
