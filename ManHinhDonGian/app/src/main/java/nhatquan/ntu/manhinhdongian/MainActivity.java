b09d29e4ad290996ef7fccac7df88df815f82de2
LvNhatQuan <nhatquanb@gmail.com>
2025-03-12 21:02:54 +0700

    Ex6_IntentDonGian

---
+++ ManHinhDonGian/app/src/main/java/nhatquan/ntu/manhinhdongian/MainActivity.java
@@ -0,0 +1,48 @@
+package nhatquan.ntu.manhinhdongian;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien(){
+        nutMH2 = findViewById(R.id.btnMH2);
+        nutMH3 = findViewById(R.id.btnMH3);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        // Tìm điều khiển nút bấm
+        TimDieuKhien();
+        //Gắn bộ lắng nghe sự kiện
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý chuyển màn hình
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý chuyển màn hình
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                startActivity(intentMH3);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
