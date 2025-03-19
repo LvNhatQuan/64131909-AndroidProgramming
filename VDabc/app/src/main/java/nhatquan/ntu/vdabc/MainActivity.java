1c91a0ff10cb9560a6521439e228e246410d6949
LvNhatQuan <nhatquanb@gmail.com>
2025-03-19 20:16:48 +0700

    Vi du ngay 17/3

--- VDabc/app/src/main/java/nhatquan/ntu/vdabc/MainActivity.java
+++ VDabc/app/src/main/java/nhatquan/ntu/vdabc/MainActivity.java
@@ -6,16 +6,38 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edt;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        edt = findViewById(R.id.edt);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edt.getText().toString().trim();
+                if(!name.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Chào" + name,Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Vui lòng nhập tên"+name,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.edt), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
