ce838ae2b7adaa332884df9a1e1e7211ad56c710
LvNhatQuan <nhatquanb@gmail.com>
2025-04-20 21:01:59 +0700

    static

--- FragmentEx_Statically/app/src/main/java/nhatquan/ntu/fragmentex_statically/ContentFragment.java
+++ FragmentEx_Statically/app/src/main/java/nhatquan/ntu/fragmentex_statically/ContentFragment.java
@@ -59,6 +59,6 @@ public class ContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        return inflater.inflate(R.layout.activity_content_fragment, container, false);
     }
 }
--- FragmentEx_Statically/app/src/main/java/nhatquan/ntu/fragmentex_statically/FooterFragment.java
+++ FragmentEx_Statically/app/src/main/java/nhatquan/ntu/fragmentex_statically/FooterFragment.java
@@ -59,6 +59,6 @@ public class FooterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_footer, container, false);
+        return inflater.inflate(R.layout.activity_footer_fragment, container, false);
     }
 }
