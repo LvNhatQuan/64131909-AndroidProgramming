347577b73fd5d54c30f998e6bc97791afb0661a9
LvNhatQuan <nhatquanb@gmail.com>
2025-05-09 09:40:32 +0700

    userside

--- Quiz/app/src/main/java/nhatquan/ntu/quiz/MainActivity.java
+++ Quiz/app/src/main/java/nhatquan/ntu/quiz/MainActivity.java
@@ -19,34 +19,56 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.sachin.quiz.Adapters.CategoryAdapter;
-import com.sachin.quiz.Models.CategoryModel;
-import com.sachin.quiz.databinding.ActivityMainBinding;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
-    FirebaseDatabase database;
-    FirebaseStorage storage;
-    CategoryAdapter adapter;
-    ArrayList<CategoryModel> list;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ImageView menu;
     View header;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.main);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        menu = findViewById(R.id.menu);
+
+        header = navigationView.getHeaderView(0);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.share){
+                    String shareBody = "Hye, I am Using best online quiz app"+"http://play.google.com/store/apps/details?id="+MainActivity.this.getPackageName();
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                    startActivity(intent);
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (item.getItemId()==R.id.rate) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id="+MainActivity.this.getPackageName())));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (item.getItemId()==R.id.privacy) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("Add Privacy Policy Link")));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                return false;
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
